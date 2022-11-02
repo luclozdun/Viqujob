cf8c5d84ae2ea116fcabe383adfadd0955ff70b6
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-11-02 00:39:54 -0500

    ad

--- ViQuJob/src/test/java/com/viqujob/jobagapi/ViQuJobApplicationTests.java
+++ ViQuJob/src/test/java/com/viqujob/jobagapi/ViQuJobApplicationTests.java
@@ -1,5 +1,7 @@
 package com.viqujob.jobagapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
